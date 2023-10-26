b67784ce75d060a3587db87af2f18ae698567442
carlosLapa <carlosferreiralapa@gmail.com>
2023-10-26 10:13:02 +0100

    added user with real email to import.sql

--- src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscatalog.services;
 
-import java.time.Instant;
-import java.util.List;
-import java.util.UUID;
-
+import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.dto.NewPasswordDTO;
+import com.devsuperior.dscatalog.entities.PasswordRecover;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -14,80 +17,76 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.dscatalog.dto.EmailDTO;
-import com.devsuperior.dscatalog.dto.NewPasswordDTO;
-import com.devsuperior.dscatalog.entities.PasswordRecover;
-import com.devsuperior.dscatalog.entities.User;
-import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
-import com.devsuperior.dscatalog.repositories.UserRepository;
-import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
-	@Value("${spring.mail.username}")
-	private String defaultSender;
+    @Value("${spring.mail.username}")
+    private String defaultSender;
 
-	@Value("${email.password-recover.uri}")
-	private String recoverUri;
+    @Value("${email.password-recover.uri}")
+    private String recoverUri;
 
-	@Value("${email.password-recover.token.minutes}")
-	private Long tokenMinutes;
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-	@Autowired
-	private PasswordRecoverRepository passwordRecoverRepository;
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Autowired
-	private EmailService emailService;
+    @Autowired
+    private EmailService emailService;
 
-	@Transactional
-	public void createRecoverToken(EmailDTO body) {
-		User user = userRepository.findByEmail(body.getEmail());
-		if (user == null) {
-			throw new ResourceNotFoundException("Email not found");
-		}
+    @Transactional
+    public void createRecoverToken(EmailDTO body) {
+        User user = userRepository.findByEmail(body.getEmail());
+        if (user == null) {
+            throw new ResourceNotFoundException("Email não encontrado");
+        }
 
-		String token = UUID.randomUUID().toString();
+        String token = UUID.randomUUID().toString();
 
-		PasswordRecover entity = new PasswordRecover();
-		entity.setToken(token);
-		entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
-		entity.setEmail(body.getEmail());
-		passwordRecoverRepository.save(entity);
+        PasswordRecover entity = new PasswordRecover();
+        entity.setToken(token);
+        entity.setExpiration(Instant.now().plusSeconds(tokenMinutes * 60L));
+        entity.setEmail(body.getEmail());
+        passwordRecoverRepository.save(entity);
 
-		String text = "Acesse o link para definir uma nova senha (válido por " + tokenMinutes + " minutos):\n\n"
-				+ recoverUri + token;
+        String text = "Acesse o link para definir uma nova senha (válido por " + tokenMinutes + " minutos):\n\n"
+                + recoverUri + token;
 
-		emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
-	}
+        emailService.sendEmail(body.getEmail(), "Recuperação de senha", text);
+    }
 
-	@Transactional
-	public void saveNewPassword(NewPasswordDTO body) {
-		List<PasswordRecover> list = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+    @Transactional
+    public void saveNewPassword(NewPasswordDTO body) {
+        List<PasswordRecover> list = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
 
-		if (list.size() == 0) {
-			throw new ResourceNotFoundException("Invalid token");
-		}
+        if (list.isEmpty()) {
+            throw new ResourceNotFoundException("Invalid token");
+        }
 
-		User user = userRepository.findByEmail(list.get(0).getEmail());
-		user.setPassword(passwordEncoder.encode(body.getPassword()));
-		userRepository.save(user);
-	}
+        User user = userRepository.findByEmail(list.get(0).getEmail());
+        user.setPassword(passwordEncoder.encode(body.getPassword()));
+        userRepository.save(user);
+    }
 
-	protected User authenticated() {
-		try {
-			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-			String username = jwtPrincipal.getClaim("username");
-			return userRepository.findByEmail(username);
-		} catch (Exception e) {
-			throw new UsernameNotFoundException("Invalid user");
-		}
-	}
+    protected User authenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return userRepository.findByEmail(username);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Invalid user");
+        }
+    }
 }
